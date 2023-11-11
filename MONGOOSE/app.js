601f64762749a1d51fe5df9ce8838e349f821373
Muhamad Nur Syami <nursami385@gmail.com>
2023-11-11 23:09:50 +0700

    change

--- MONGOOSE/app.js
+++ MONGOOSE/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const cors = require("cors");
 const db = require("./config/db");
 const allRoutes = require("./routes");
 const app = express();
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 db.then(() => {
   console.log("database connect mongodb");
 }).catch((err) => console.log(`gagal connect mongodb ${err}`));
+app.use(cors());
 // EXPRESS.JSON  HARUS PALING ATAS SEBELUM SEMUA ROUTING
 app.use(express.json());
 app.use(allRoutes);
